bdc1fcb126477af10dd6abc1f7790dd8744e5784
y-matsuno-u <chyu24003@g.nihon-u.ac.jp>
2025-03-01 20:02:21 +0900

    webhook検証関数作成

--- translation_test/test_api/views.py
+++ translation_test/test_api/views.py
@@ -1,5 +1,25 @@
+import os
+from django.http import JsonResponse, HttpResponseForbidden
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
+from validating_webhook import verify_signature
+from dotenv import load_dotenv
 
-# Create your views here.
-def create_webhook(request):
-    return render(request, "test_api/create_webhook.html")
+
+
+def validate_webhook(request):
+
+    #.envファイルのパスを指定して読み込む
+    load_dotenv('.env')
+    secret_token = os.getenv('WEBHOOK_SECRET')
+
+    #リクエストヘッダーからx-hub-signature-256を取得
+    signature_header = request.META.get("HTTP_X_HUB_SIGNATURE_256")
+
+    try:
+        #署名検証を実施
+        verify_signature(request.body, secret_token, signature_header)
+    except PermissionDenied as e:
+        return HttpResponseForbidden(str(e))
+    
+    return render(request, "test_api/validate_webhook.html")
